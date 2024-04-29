7b8ab4cfe6f50e84770847c1090838d7a5d4872f
Tyler Hunt <tyhunt82@users.noreply.github.com>
2024-04-29 06:57:10 +0000

    tweak ui

--- app.py
+++ app.py
@@ -1,43 +1,44 @@
 from nicegui import ui
 from nicegui import Tailwind
 
-with ui.header().classes(replace='flex text-lg min-h-18 bg-gray-800')as header:
-    with ui.row().classes('flex'):
-        ui.button(on_click=lambda: left_drawer.toggle(), icon='menu').tailwind('bg-gray-800')
-        ui.label('The Labs').classes('text-xl font-bold')
+with ui.header().classes(replace='text-lg min-h-12 bg-gray-800')as header:
+    with ui.grid(columns=3).classes('flex'):
+        ui.button(on_click=lambda: left_drawer.toggle(), icon='menu').props('flex-none w-14 h-12')
+        ui.label('The Labs').classes('align-middle text-white text-lg font-bold ')
+        
+        with ui.tabs().classes('grow text-xl font-bold h-12') as tabs:
+            ui.tab('A')
+            ui.tab('B')
+            ui.tab('C')
+
+        ui.button('Login', 
+                on_click=lambda: ui.notify('Your logged in')
+                ).classes('''flex-none w-20 h-12 bg-gray-800 text-white px-4 py-2  rounded-md''').props('flat')
+        
+        ui.label('or').classes('text-sm text-gray-400')
+
         ui.button('Sign in', 
                 on_click=lambda: ui.notify('Your signed in')
-                ).classes('''flex justify-end bg-gray-800 text-white px-4 py-2 float-right rounded-md''').props('flat')
-
-    
-
-
-
-with ui.footer(value=False).classes('bg-white') as footer:
-    with ui.row().classes('center'):
-        ui.label('Footer').classes('black')
-    ui.label('Label B').tailwind('drop-shadow', 'font-bold', 'text-green-600')
+                ).classes('''flex-none w-20 h-12 bg-gray-800 text-white px-4 py-2  rounded-md''').props('flat')
 
 with ui.left_drawer().classes('bg-gray-100') as left_drawer:
     ui.label('Left menu')
-    with ui.tabs() as tabs:
-        ui.tab('A')
-        ui.tab('B')
-        ui.tab('C')
+    ui.separator()
+    with ui.expansion('Expand!', icon='work').classes('w-full'):
+        ui.label('inside the expansion')
+   
 
 with ui.page_sticky(position='bottom-right', x_offset=20, y_offset=20):
-    ui.button(on_click=footer.toggle, icon='contact_support')
-
+    pass
     
 
 with ui.tab_panels(tabs, value='A').classes('w-full'):
-
-
     with ui.tab_panel('A'):
         ui.label('Content of A')
     with ui.tab_panel('B'):
         ui.label('Content of B')
     with ui.tab_panel('C'):
         ui.label('Content of C')
+
 
 ui.run()
